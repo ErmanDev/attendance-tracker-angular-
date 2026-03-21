a9f3a1c0ad65bf9d2f77a8666e3ac88370fdda67
ErmanDev <ermanfaminiano020@gmail.com>
2026-03-22 02:09:22 +0800

    new init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'home', component: Home },
+  { path: 'about', component: About },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
